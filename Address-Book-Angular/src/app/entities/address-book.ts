bc4fb38d9cac56da358943cf55e4e1dbddb72e77
anzar shaikh <anzarshaikh373@gmail.com>
2021-11-11 10:48:33 +0530

    address book angular project completed

---
+++ Address-Book-Angular/src/app/entities/address-book.ts
@@ -0,0 +1,14 @@
+export class AddressBook {
+
+  public id : number;
+
+  public name : string;
+  public phone_no : string;
+  public email_id : string;
+  public password : string;
+  public address : string;
+  public city : string;
+  public state : string;
+  public zip_code : string;
+
+}
